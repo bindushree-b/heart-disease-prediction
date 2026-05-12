1491361286d60291f5adf519e8f047a95dc7080a
bindushree-b <bindushreeb2104@gmail.com>
2026-05-12 11:31:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Heart Disease Prediction
+
+This is a Machine Learning web application built using Streamlit.  
+The model predicts the possibility of heart disease based on health parameters.
+
+## Technologies Used
+- Python
+- Streamlit
+- Scikit-learn
+- Pandas
+- NumPy
+
+## Run Locally
+
+```bash
+pip install -r requirements.txt
+streamlit run app.py
---
+++ app.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import pickle as pk
+
+# model = pk.load(open('D:\Heart Disease Prediction\Heart_disease_model.pkl','rb'))
+model = pk.load(open("Heart_disease_model.pkl", "rb"))
+
+data = pd.read_csv('D:\Heart Disease Prediction\heart_disease.csv')
+
+st.header('hear Disease Predictor')
+
+gender = st.selectbox('Choode Gender', data['Gender'].unique())
+if gender == 'Male' :
+    gen = 1
+else:
+    gen = 0
+
+age = st.number_input("Enter Age")
+currentSmoker = st.number_input("Is Patient currentSmoker")
+cigsPerDay = st.number_input("Enter cigsPerDay")
+BPMeds = st.number_input("Is Patient on BPMeds")
+prevalentStroke = st.number_input("Is Patient Had stroke")
+prevalentHyp = st.number_input("Enter prevalentHyp status")
+diabetes = st.number_input("Enter diabetes status")
+totChol = st.number_input("Enter totChol")
+sysBP = st.number_input("Enter sysBP")
+diaBP = st.number_input("Enter diaBP")
+BMI = st.number_input("Enter BMI")
+heartRate = st.number_input("Enter heartRate")
+glucose = st.number_input("Enter glucose")
+if st.button('Predict'):
+    input = np.array([[gen,age,currentSmoker,cigsPerDay,BPMeds,
+                   prevalentStroke,prevalentHyp,diabetes,totChol,sysBP,
+                   diaBP,BMI,heartRate,glucose]])
+    output = model.predict(input)
+    if output[0] == 0:
+        stn = 'Patient is Healthy, No heart Disease'
+    else:
+        stn = 'Patient May have Heart Disease'
+    st.markdown(stn)
+        
